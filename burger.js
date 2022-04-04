c6f2ec27805e8b6fab2c1d050032125afa0e56bc
unknown <egkirpichev@gmail.com>
2022-04-04 19:19:21 +0300

    pushing finalized files for review

---
+++ burger.js
@@ -0,0 +1,12 @@
+// burger menu
+
+let hamburger = document.querySelector(".hamburger");
+let headerNav = document.querySelector(".header__nav");
+let navLink = document.querySelector(".navlink");
+let navLogo = document.querySelector("header .logo")
+
+hamburger.addEventListener("click", function() {
+	headerNav.classList.toggle("expanded");
+	hamburger.classList.toggle("expanded");
+	navLogo.classList.toggle("expanded");
+});
