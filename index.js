1fccba8f3aaa0b3c8a3976a29af2a5b7f5c23f1d
unknown <egkirpichev@gmail.com>
2022-04-19 01:24:26 +0300

    pushing HW17

--- index.js
+++ index.js
@@ -1,184 +1,208 @@
+// ### NORMAL level
+
 // #### Task 1 💻
 
-// let str = 'true'
-// let bool = false
-// let num = 17
-// let undef = undefined
-// let nu = null
+// const getSum = function (b) {
+// 	let sum = 0
+// 	for (i = 0; i <= b; i++) {
+// 		sum = sum + i
+// 	} return sum
+// } 
 
-// console.log(typeof(str), typeof(bool), typeof(num), typeof(undef), typeof(nu)) 
+// console.log(getSum(100))
 
 // #### Task 2 💻
 
-// let height = 15
-// let width = 20
+// const overCharge = function (loanBody, interest, period) {
+// 	let monthlyPayment = Math.round(loanBody * (interest/12/100*((1+interest/12/100)**(period*12))) / (((1 + interest/12/100)**(period*12)-1)))
+// 	console.log(`ежемесячный платеж ${monthlyPayment}`)
+// 	return monthlyPayment*12*period - loanBody 
+// } 
 
-// if (height > width) {
-// 	console.log(height)
-// } else {
-// 	console.log(width)
-// }
+// console.log(overCharge(10000, 17, 5))
 
 // #### Task 3 💻
 
-// for (let i = 0; i <= 20; i++) {
-// 	if (i % 3 === 0) {
-// 		console.log(i)
-// 	}
-// }
+// const trimString = function (str, strStart, strEnd) {
+// 	str = str.split(" ")
+// 	strStartIndex = 0
+// 	strStartIndex = 0
+// 	for (i = 0; i < str.length; i++) {
+// 		if (str[i].includes(strStart)) {
+// 			strStartIndex = i
+// 		} else if (str[i].includes(strEnd)) {
+// 			strEndIndex = i
+// 		}
+// 	} return str.slice(strStartIndex++, strEndIndex)
+// } 
+
+// console.log(trimString("Что-то выдавало в Штирлице советского разведчика. Не то мужественный профиль, не то решительная походка, не то волочащийся за ним парашют.", "в", "парашют."))
 
 // #### Task 4 💻
 
-// let key = true;
-// let documents = true;
-// let pen = true;
-// let apple = false;
-// let orange = true;
+// const getSumNumbers = function (a) {
+// 	a = String(a).split("")
+// 	let sum = 0
+// 	for (i=0; i < a.length; i++){
+// 		sum = sum + Number(a[i]) 
+// 	} return sum
+// } 
 
-// if ((key && documents && pen) && (apple || orange)) {
-// 	console.log("Можно идти на работу!")
-// }
+// console.log(getSumNumbers(2022)) 
 
 // #### Task 5 💻
 
-// let num = prompt("Введите число");
+// const getSum = function (a, b) {
+// 	let sum = 0
+// 	for (i = a; i <= b; i++) {
+// 		sum = sum + i
+// 	} return sum
+// } 
 
-// if ((+ num % 5 === 0) && ((+ num % 3 === 0))) {
-// 	alert("FizzBuz");
-// } else if (+ num % 5 === 0) {
-// 	alert("Fizz");
-// } else {
-// 	alert("Buz");
-// }
+// console.log(getSum(1, 0)) // 1 + 0 = 1
+// console.log(getSum(1, 2)) // 1 + 2 = 3
+// console.log(getSum(0, 1)) // 0 + 1 = 1
+// console.log(getSum(1, 1)) // 1 Since both are same
+// console.log(getSum(-1, 0))// -1 + 0 = -1
+// console.log(getSum(-1, 2))// -1 + 0 + 1 + 2 = 2
 
 // #### Task 6 💻
 
-// let age = prompt("Введите ваш возраст")
+// const foo = () => {return console.log(foo.name)}
+// const boo = () => {return console.log(boo.name)}
 
-// if ((+ age >= 16) && ((+ age < 18))) {
-// 	alert("Можешь выкурить сигаретку, только маме не говори");
-// } else if (+ age < 18) {
-// 	alert("Пей колу");
-// } else {
-// 	alert("Попей пивка");
-// }
+// const fooboo = function (condition, foo, boo) {
+// 	if (!!condition) {
+// 		return foo()
+// 	} else return boo()
+// } 
 
-// #### Task 7 💻
-
-// let str = prompt("Введите сторону света")
-
-// switch (str) {
-// 	case "юг":
-// 		alert("на юг пойдешь счастье найдешь");
-// 		break;
-// 	case "север":
-// 		alert("на север пойдешь много денег найдешь");
-// 		break;
-// 	case "запад":
-// 		alert("на запад пойдешь верного друга найдешь");
-// 		break;
-// 	case "восток":
-// 		alert("на восток пойдешь разработчиком станешь");
-// 		break;
-// 	default:
-// 		alert("Попробуйте еще раз")				
-// }
+// fooboo(false, foo, boo)
 
 // ### ADVANCED level
 
 // #### Task 1 👨‍🏫 
 
-// let user = "пОлИнА нАбЕрЕжНаЯ";
-// function rework (user) {
-// 	user = user.toLowerCase();
-// 	user = user.split("")
-// 	console.log(user)
-// 	user[0] = user[0].toUpperCase()
-// 	console.log(user)
-// 	for (i = 1; i < user.length; i++) {
-// 		if (user[i] === " ") {
-// 			user[i+1] = user[i+1].toUpperCase()
-// 		}
-// 	} 
-// 	return user.join("")
+// const triangle = function(a, b, c) {
+// 	if (a > 0 && b > 0 && c > 0) {
+// 		if (a + b > c && a + c > b && b + c > a) {
+// 		return true
+// 		} else return false
+// 	} else return alert("Длина стороны треугольника должна быть положительной")
 // }
-
-// alert(`Привет, ${rework(user)}!`)
+// console.log(triangle(-1, 15, 15))
 
 // #### Task 2 👨‍🏫 
 
-// let num = prompt("Введите число");
-// let substr = prompt("Сколько отнять от предыдущего результата");
-// let plus = prompt("Сколько прибавить от предыдущего результата");
-// let multiply = prompt("На сколько умножить от предыдущего результата") 
-// let divide = prompt("На сколько разделить от предыдущего результата")
 
-// let result = (((Number(num) - Number(substr)) + Number(plus)) * Number(multiply)) / Number(divide)
+	let budget = Number(prompt("Введите сумму вашего бюджета")) // запрашиваем бюджет
+	let currency = false // далее уточняе валюту накоплений
+	if (currency = confirm("Вы ввели сумму накоплений в рублях?")) {
+			currency = "RUB"
+		}	else if (currency = confirm("Вы ввели сумму накоплений в долларах?")){
+			currency = "USD"
+		}	else if	(currency = confirm("Вы ввели сумму накоплений в евро?")){
+			currency = "EUR"} else {
+				currency = "RUB";
+				budget = Number(prompt("Пожалуйста, введите сумму вашего бюджета в рублях"))
+			}
+	
+	const phonePriceRUB = 800 //задаем переменные цен, НДС и курсов
+	const casePriceRUB = 20
+	const taxRateVAT = 0.2
+	let	exchangeRateEUR = 3
+	let	exchangeRateUSD = 2.8
 
-// alert(`(((${num} - ${substr}) + ${plus}) * ${multiply}) / ${divide} = ${result}`)
+	let bill = {} //создаем объект для последующего вывода данных 
+	let purchasePrice = 0
+	let budgetLeft = 0
+	let purchaseQuantity = 0
+
+	
+	const purchasePriceCalc = function ()	{if (currency === "RUB") {for (let i = 1; purchasePrice <= budget; i++) { //расчет учитывает курс валюты 
+		purchasePrice = Number(((1+taxRateVAT)*(phonePriceRUB + casePriceRUB)*i).toFixed(2)) //округляем сумму чека до двух знаков после запятой
+		purchaseQuantity = i
+	}} else if (currency === "EUR") {for (let i = 1; purchasePrice <= budget; i++) {
+		purchasePrice = Number(((1+taxRateVAT)*(phonePriceRUB/exchangeRateEUR + casePriceRUB/exchangeRateEUR)*i).toFixed(2))
+		purchaseQuantity = i
+	}} else {for (let i = 1; purchasePrice <= budget; i++) {
+		purchasePrice = Number(((1+taxRateVAT)*(phonePriceRUB/exchangeRateUSD + casePriceRUB/exchangeRateUSD)*i).toFixed(2))
+		purchaseQuantity = i
+	}}
+	purchasePrice = Number(((1+taxRateVAT)*(phonePriceRUB/exchangeRateEUR + casePriceRUB/exchangeRateEUR)*purchaseQuantity).toFixed(2))
+	budgetLeft = Number((budget - purchasePrice).toFixed(2))
+
+	return bill = { //возвращаем данные через объект
+		purchasePrice: purchasePrice,
+		budgetLeft: budgetLeft,
+		purchaseQuantity: purchaseQuantity
+	}
+}
+
+	bill = purchasePriceCalc() //вызываем функцию, перезаписываем ключи объекта 
+
+	if (bill[purchaseQuantity] === 0) { // проверяем сможет ли человек совершить покупку
+		alert("Ваш бюджет не позволяет совершить ни одной покупки")
+	} else {
+		alert(`С вашим бюджетом вы сможете купить следующее количество пар смартфон+чехол: ${purchaseQuantity}. Сумма чека с учетом НДС составит: ${purchasePrice}${currency}. Остаток денежных средств на вашем счете будет равен ${budgetLeft}${currency}`)
+	}
+
+	console.log(budget)
+	console.log(currency)
+	console.log(purchasePriceCalc())
 
 // #### Task 3 👨‍🏫
 
-// let arr = [];
+// const chocolate = function(n, m) {
+// 	let numBreaks = 0
+// 	if (n > 0 && m > 0 && n % 1 === 0 && m % 1 === 0) {
+// 		for (i = 1; i < n * m; i++) {
+// 			numBreaks += 1 
+// 		}
+// 	} else alert("Введите положительные целые числа")
+// 	return alert (`Для разделения плитки потребуется ${numBreaks} надломов`)
+// }
+// console.log(chocolate(NaN, NaN))
 
-// for (let i = 0; i<=5; i++){
-// 	arr.push("*")
-// 	console.log(arr)
+// #### Codewars
+
+// 1. https://www.codewars.com/kata/55a5befdf16499bffb00007b
+
+// function add(a,b){
+// 	return a + b
 // }
 
-// let str = "";
-
-// for (let i = 0; i<=5; i++){
-// 	str = str + "*"
-// 	console.log(str)
+// function divide(a,b){
+// 	return a / b
 // }
 
-// #### Task 4 👨‍🏫
-
-// https://www.codewars.com/kata/54ba84be607a92aa900000f1
-
-// function isIsogram(str){
-//   str = str.toLowerCase()
-//   let arr = str.split("")
-//   for(let i = 0; i<arr.length; i++) {
-//     for(let j = i + 1; j<arr.length; j++) {
-//       if (arr[i] == arr[j]) {
-//       return false
-//   } 
-//   }  
-//   } 
-//   return true
+// function multiply(a,b){
+// 	return a * b
 // }
 
-// https://www.codewars.com/kata/53dc23c68a0c93699800041d
-
-// function smash (words) {
-// 	words = words.join(" ")
-// 	 if (words.length = 1) {
-// 	 return words.toString()
-// 	 } else if (words.length = 0){
-// 	 return ""
-// 	 } else for (let i = 0; i < words.length; i++ ){
-// 		 words[i] = words[i] + " "
-// 	 } 
-// 	 return words.toString()
-//  };
-
-//  https://www.codewars.com/kata/55a70521798b14d4750000a4
-
-//  function greet(name){
-// 	return `Hello, ${name} how are you doing today?` 
-//  }
-
-//  https://www.codewars.com/kata/57e1e61ba396b3727c000251
-
-//  function stringClean(s){
-//   s = s.split("")
-//   for (let i = 0; i < s.length; i++) {
-//     if (s[i] === "0" || s[i] === "1" || s[i] === "2" || s[i] === "3" || s[i] === "4" || s[i] === "5" || s[i] === "6" || s[i] === "7" || s[i] === "8" || s[i] === "9") {
-//         s[i] = ""
-//         } 
-//   }
-// return s.join("")
+// function mod(a,b){
+// 	return a % b
+// }
+ 
+// function exponent(a,b){
+// 	return a ** b
+// }
+	
+// function subt(a,b){
+// 	return a - b
 // }
 
+// 2. https://www.codewars.com/kata/559ac78160f0be07c200005a
+
+// function nameShuffler(str){
+//   let arr = str.split(" ").reverse()
+//   return arr.join(" ")
+// }
+
+// 3. https://www.codewars.com/kata/50654ddff44f800200000007
+
+// function solution(a, b){
+//   if (a.length > b.length) {
+//     return b + a + b
+//   }else return a + b + a
+// }
